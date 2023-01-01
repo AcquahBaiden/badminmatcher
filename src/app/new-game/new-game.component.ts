8c69b3badc10bc3bf92a91331ce97551fbb83a30
AcquahBaiden <jbaiden@live.com>
2022-12-31 23:47:50 -0500

    Added player interface, component feature for adding new line

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../interfaces/player.interface';
 
 @Component({
   selector: 'app-new-game',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-game.component.css']
 })
 export class NewGameComponent implements OnInit {
-
+  players: Player [] = [{name:'',ranking:0}]
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onAddPlayer(){
+    this.players.push({name:'', ranking:0})
   }
 
 }
