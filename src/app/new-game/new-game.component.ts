7757f145106d9369c2329f6780695274c5f1feac
AcquahBaiden <jbaiden@live.com>
2023-01-02 21:12:31 -0500

    Added player list

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -7,7 +7,12 @@ import { Player } from '../interfaces/player.interface';
   styleUrls: ['./new-game.component.css']
 })
 export class NewGameComponent implements OnInit {
-  players: Player [] = []
+  players: Player [] = [
+    {name:"John Smith", ranking:23},
+    {name:"Jack Ryan", ranking:70},
+    {name:"Sarah Snow", ranking:120},
+    {name:"Philip Water", ranking:330},
+  ]
   constructor() { }
 
   ngOnInit(): void {
