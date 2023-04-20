ed3f23681dbe7d632cb5d0281e94a42aaed85b2f
Joe A Baiden <jbaiden@live.com>
2023-04-19 22:42:38 -0400

    Added database create public data

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,20 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { environment as dev } from 'src/environments/environment';
 import { environment as prod } from 'src/environments/environment.prod';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor(private database: AngularFireDatabase, private auth: AngularFireAuth) { }
+  constructor(private database: AngularFireDatabase, 
+    private auth: AngularFireAuth,
+    private log: LoggerService) { }
 
   async createNewGame(){
-    // return new Promise(()=>'here we go')
-    const pushId = await this.database.createPushId();
-    
+    const pushId = this.database.createPushId();
+    const user = await this._user()
+    let path:string = ''
+    if (user){
+      path = user.uid
+    }else{
+      path = isDevMode() ? dev.userId: prod.userId
+    }
+    try {
+      this.database.list(path).set(pushId,{
+        uid: pushId,
+        title: 'New game'
+      })
+    } catch (error) {
+      this.log.error({
+        message: "Database error",
+        extras: error
+      })
+    }
     return pushId
   }
 
@@ -24,19 +43,21 @@ export class AppService {
      */
     //get user id
     // or return defined createPusId path for environment
-    const user = this._user
+    const user = await this._user()
     if(user){
       return user
     }
-    return process.env.production ? prod.userId : dev.userId
+    // return isDevMode() ? dev.userId: prod.userId
+    return 'asdf'
   }
 
   async _user(){
-    const user = await this.auth.currentUser
-    if(user){
-      return user.uid
-    }else{
-      return null
-    }
+    return this.auth.currentUser
+    // const user = await this.auth.currentUser
+    // if(user){
+    //   return user.uid
+    // }else{
+    //   return null
+    // }
   }
 }
